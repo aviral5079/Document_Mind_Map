32e655b09ddd7c9c60d65c3d70ea98b1022c51a0
avi5079 <aviral.srivastava2102@gmail.com>
2023-12-16 15:30:49 +0530

    mindmap-init

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import AppBar from "./components/AppBar";
 import SideBar from "./components/SideBar";
+import MainBar from "./components/MainBar";
 
 function App() {
   return (
     <div className="app">
       <AppBar />
       <SideBar />
+      <MainBar />
     </div>
   );
 }
